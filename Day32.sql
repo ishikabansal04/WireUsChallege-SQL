56ae25723acd876b6563d29817512767312c9c78
ishikabansal04 <49348251+ishikabansal04@users.noreply.github.com>
2020-10-20 18:10:54 +0530

    Update Day32.sql

--- Day32.sql
+++ Day32.sql
@@ -1,20 +1,20 @@
--- 1 ans:)
--- Firstname      EmpSalary      
--- Maithili        50000
--- Robert          45000
--- Henry           40000
+1 ans:)
+Firstname      EmpSalary      
+Maithili        50000
+Robert          45000
+Henry           40000
 
 
--- 2 ans:)
--- 3
+2 ans:)
+3
 
 
--- 3 ans:)
--- Designation     Sum_Salary
--- Manager           214500
--- Clerk             135000
+3 ans:)
+Designation     Sum_Salary
+Manager           214500
+Clerk             135000
 
 
--- 4 ans:)
--- COUNT
--- 32000
+4 ans:)
+COUNT
+32000
