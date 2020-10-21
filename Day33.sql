47a998df034aaf6053db05e5c41c3a1b8d34f82a
ishikabansal04 <49348251+ishikabansal04@users.noreply.github.com>
2020-10-21 10:08:22 +0530

    Adding code for Day33

---
+++ Day33.sql
@@ -0,0 +1,8 @@
+ques 1) Which of the following is not a atype of DBMS?
+ans 1) loop databases
+
+ques 2) Which of the following is a type of inner join?
+ans 2) All of these
+
+ques 3)Which of the following is not for TRUNCATE command is sql?
+ans 3) Truncate is not a DDL command.
